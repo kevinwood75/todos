360c9f19d01718aaafb5aacfa8474a5b6b472ba3
Kevin Wood <kevin.wood75@gmail.com>
2019-02-02 16:19:42 -0500

    update

--- src/App.js
+++ src/App.js
@@ -28,12 +28,27 @@ class App extends Component {
 
   }  
 
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
 
+    }) });
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id )
+    ! == id] })
+  }
 
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} 
+          delTodo={this.delTodo} />
       </div>
     );
   }
